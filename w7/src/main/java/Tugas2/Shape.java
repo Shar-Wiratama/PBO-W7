538a8c0676e95f27d16b98d2a90956f1d6ba9a28
Shar-Wiratama <80728579+Shar-Wiratama@users.noreply.github.com>
2021-10-30 14:55:11 +0700

    Update Shape.java

--- w7/src/main/java/Tugas2/Shape.java
+++ w7/src/main/java/Tugas2/Shape.java
@@ -1,35 +1,36 @@
 package Tugas2;
 
-
 public class Shape {
-    private String color;
-    private boolean filled;
-    
-    public Shape(){
-        color =  "green";
-        filled = true;
-    }
-    public Shape(String color, boolean filled){
-        this.color=color;
-        this.filled=filled;
-    }
-    public void setFilled(){
-        this.filled=filled;
-    }
-    public boolean isFilled(){
-        return filled;
-    }
-     public void setColor(){
-        this.color=color;
-    }
-    public String getColor(){
-        return color;
-    }
-    public String toString(){
-    if(isFilled()==0){
-        return "A shape with color "+this.color+ " and filled";
-    }else{
-        return "Shape[color= "+color+" filled="+filled+"]";
-    }
-    
+	private String color;
+	private boolean filled;
+	//Constructor shape 1
+	public Shape() {
+		color = "green";
+		filled = true;
+	}
+        //Constructor shape 2
+	public Shape(String color,boolean filled) {
+		this.color = color;
+		this.filled = filled;
+	}
+        //Returns the filled
+	public boolean isFilled() {
+		return filled;
+	}
+        //set for filled
+	public void setFilled(boolean filled) {
+		this.filled = filled;
+	}	
+        //Returns for the color
+	public String getColor() {
+		return color;
+	}
+        //set for color
+	public void setColor(String color) {
+		this.color = color;
+	}
+        //output for shape
+	public String toString() {
+		return "Shape[color= "+color+", filled="+filled + "]";
+	}
 }
