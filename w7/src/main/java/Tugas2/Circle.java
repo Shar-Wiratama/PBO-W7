fe15c04ef30d94a7961062c1316a5aeadeeec121
Shar-Wiratama <80728579+Shar-Wiratama@users.noreply.github.com>
2021-10-30 14:53:33 +0700

    Update Circle.java

--- w7/src/main/java/Tugas2/Circle.java
+++ w7/src/main/java/Tugas2/Circle.java
@@ -1,34 +1,40 @@
 package Tugas2;
 
-public class Circle {
-    private double radius;
-    
-    public Circle(){
-        super();
-        setRadius(1.0);
-    }
-    public Circle(double radius){
-        super();
-        this.setRadius(radius);
-    }
-    public Circle(double radius, boolean filled, String color){
-        super(color,filled);
-        this.setRadius(radius);
-    }
-    public void setRadius(double radius){
-        this.radius=radius;
-    }
-    public double getRadius(){
-        return radius;
-    }
-    public double getArea(){
-        return Math.PI*radius*radius;
-    }
-    public double getPerimeter(){
-        return 2*Math.PI*radius;
-    }
-    @Override
-    public String toString(){
-        return "Circle[ "+super.toString()+", radius= "+radius+"]";
-    }
+public class Circle extends Shape{
+private double radius;
+	//constructor circle 1
+	public Circle() {
+		super();
+		setRadius(1.0);
+	}
+        //constructor circle 2
+	public Circle(double radius) {
+		super();
+		this.setRadius(radius);
+	}
+        //constructor circle 3
+	public Circle(double radius,String color,boolean filled) {
+		super(color,filled);
+		this.setRadius(radius);
+	}
+        //return radius
+	public double getRadius() {
+		return radius;
+	}
+        //set the radius
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+       //return the formula of area for circle
+	public double getArea() {
+		return radius*radius*Math.PI;
+	}
+        //return the formula of pheriperal for circle
+	public double getPerimeter() {
+		return radius*2*Math.PI;
+	}
+        //output for circle
+	public String toString() {
+		return "Circle["+super.toString()+", radius= "+radius+"]";
+	}
 }
