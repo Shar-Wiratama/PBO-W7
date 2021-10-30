4e685b1f31c3a7adffbe1a1e8d8826b3396426f0
Shar-Wiratama <80728579+Shar-Wiratama@users.noreply.github.com>
2021-10-30 14:55:46 +0700

    Update Square.java

--- w7/src/main/java/Tugas2/Square.java
+++ w7/src/main/java/Tugas2/Square.java
@@ -1,46 +1,29 @@
 package Tugas2;
 
-public class Square extends Rectangle {
-    public Square(){
-        super();
-    }
-    public Square(double side){
-        super(side,side);
-    }
-    public Square(double side,String color,boolean filled) {
+public class Square extends Rectangle{
+	//constructor square 1
+        public Square() {
+		super();
+	}
+        //constructor square 2
+	public Square(double side) {
+		super(side,side);
+	}
+        //constructor square 3
+	public Square(double side,String color,boolean filled) {
 		super(side,side,color,filled);
-    }
-    public double getSide() {
-    	return super.getLength();
-    }
-    public void setSide(double side) {
-	super.setLength(side);
-	super.setWidth(side);
-    }
-    public double getArea(){
-        return getSide()*getSide();
-    }
-    public double getPerimater(){
-        return 4*getSide();
-    }
-    @Override
-    public void setWidth(double side){
-        if(super.setWidth(side)==super.setLength(side)){
-            super.setWidth(side);
-        }else{
-            System.out.println("Can't be created because both side aren't same size");
-        }
-    }
-    @Override
-    public void setLength(double side){
-        if(super.setWidth(side)==super.setLength(side)){
-            super.setLength(side);
-        }else{
-            System.out.println("Can't be created because both side aren't same size");
-        }
-    }
-    @Override
-    public String toString() {
-	return "Rectangle["+super.toString()+"side"+getSide()"]";
-    }
+	}
+        //return side that inherit from length in rectangle.java
+	public double getSide() {
+		return super.getLength();
+	}
+        //set the side from rectangle.java
+	public void setSide(double side) {
+		super.setLength(side);
+		super.setWidth(side);
+	}
+        //output for square
+	public String toString() {
+		return "Square["+super.toString()+"]";
+	}
 }
